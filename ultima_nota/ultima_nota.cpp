3d2d505eedf22824376bdf4be954241b5c7025ef
Cristopher <cristopher.almonacid22@gmail.com>
2020-12-09 17:13:13 -0300

    entrega actividad 5

---
+++ ultima_nota/ultima_nota.cpp
@@ -0,0 +1,38 @@
+// Librerías
+
+//Variables Globales
+
+//Función main
+
+int main (){
+	
+	float nota2;
+	float nota1;
+	float tarea1;
+	float tarea2;
+	float tarea3;
+	float actitudinal;
+	
+	printf("Nota Prueba 1:\n");
+	scanf("%f", &nota1);
+	printf("Nota Tarea 1\n");
+	scanf("%f", &tarea1);
+	printf("Nota Tarea 2\n");
+	scanf("%f", &tarea2);
+	printf("Nota Tarea 3\n");
+	scanf("%f", &tarea3);
+	printf("Nota Actitudinal\n");
+	scanf("%f", &actitudinal);
+	
+	tarea1 / 10 = tarea1
+	tarea2 / 10 = tarea2
+	tarea3 / 10 = tarea
+	nota1 / 10 * 2.5 = nota1
+	actitudinal / 10 = actitudinal
+	nota2 = nota2 / 10 * 3.5
+	40 = 100 - tarea1 - tarea2 - tarea3 - nota1 - actitudinal -nota2
+	
+	print("La nota mínima necesitada para aprobar el ramo será de %f/n", nota2);
+	 
+	 return 0;
+	 }
