cc8d35979c75bd9df11081c6f8bee9ac46c2243f
Cristopher <cristopher.almonacid22@gmail.com>
2020-12-09 16:45:40 -0300

    actividad4 realizada

--- círculo/Actividad4.cpp
+++ círculo/Actividad4.cpp
@@ -6,16 +6,15 @@
 // Función main
 
 int main (){
-    float numero1;
-    float numero2;
-    float producto;
+    float radio;
+    float perimetro;
+    float area;
 
-    printf("ingrese el primer número\n");
-    scanf("%f", &numero1);
-    printf("ingrese el segundo número\n");
-    scanf("%f", &numero2);
-    producto = numero1 * numero2;
-    printf("E  producto de ambos números es: %f\n", producto);
+    printf("ingrese el valor del área de la circunferencia\n");
+    scanf("%f", &radio);
+    perimetro = radio * 3.16 * 2;
+    area = radio * 3.16 * radio;
+    printf("El perímetro de la circuenferancia es: %f. Y el área es %f\n", perimetro, area);
 
     return 0;
 }
